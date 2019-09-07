97f621b86f3f555eb9df54ff996b3366a5e3087c
KajeMusic <github@kajemusic.ir>
2019-09-07 22:00:38 +0430

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -93,8 +93,7 @@ def callback_inline(call):
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text="👇")
         if re.match('(sendVC:).*',call.data):
                 video = call.data.replace('sendVC:','')
-                sendvid = open('./Music/'+str(video)+'-Music.mp4')
-                bot.send_video(call.message.chat.id, sendvid,supports_streaming=True)
+                bot.send_video(call.message.chat.id, open('./Music/'+str(video)+'-Music.mp4'),supports_streaming=True)
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text="👇")
 #######################################################################################
 
